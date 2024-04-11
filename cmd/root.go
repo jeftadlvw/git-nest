00968c4cc8d1ff4e309e05e52d8ee918e314ab96
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-04-11 21:25:43 +0200

    ♻️ moved rootCmd variable declaration into Execute()

--- cmd/root.go
+++ cmd/root.go
@@ -7,17 +7,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:     constants.ApplicationName,
-	Version: constants.Version(),
-	Short:   "Nest external repositories into your project without git knowing.",
-	Long: `git-nest is a git command line extension for nesting external repositories
+func Execute() int {
+
+	var rootCmd = &cobra.Command{
+		Use:     constants.ApplicationName,
+		Version: constants.Version(),
+		Short:   "Nest external repositories into your project without git knowing.",
+		Long: `git-nest is a git command line extension for nesting external repositories
 in your project without your parent repository noticing, using native features
 and configurations files.`,
-	Run: internal.PrintUsage,
-}
-
-func Execute() int {
+		Run: internal.PrintUsage,
+	}
 
 	configureRootCommand(rootCmd)
 
