fbcd04d6bc907ef215684ac57db8095b9267c06e
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-04-11 21:36:59 +0200

    🚧 file io for nestmodules.toml and .git/info/exclude

---
+++ io/config.go
@@ -0,0 +1,5 @@
+package io
+
+// TODO read (configuration) file
+
+// TODO extract configuration section, append submodule toml configuration and write to configuration file
---
+++ io/exclude.go
@@ -0,0 +1,3 @@
+package io
+
+// TODO extract git-nest configuration part location from .git/info/exclude and override it with new configuration
