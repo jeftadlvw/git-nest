4fa8fa4bed448c931eeadac70f19886b0ad5ae5e
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-04-15 14:45:35 +0200

    ✨ insert a string into another string at set determination strings

--- utils/string_insert.go
+++ utils/string_insert.go
@@ -1,36 +1,11 @@
-package io
+package utils
 
 import (
-	"bytes"
 	"fmt"
-	"github.com/jeftadlvw/git-nest/models"
 	"strings"
-	"text/template"
 )
 
-const startString string = "# git-nest configuration start"
-const endString string = "# git-nest configuration end"
-
-const excludeTemplate string = `# This part influences how git handles nested modules using git-nest.
-# Do not touch except you know what you are doing!
-{{- range . }}
-	{{ .Path }}
-{{- end }}`
-
-// TODO extract git-nest configuration part location from .git/info/exclude and override it with new configuration
-func FmtSubmodulesGitExclude(submodules []models.Submodule) string {
-	buffer := bytes.NewBufferString("")
-
-	t := template.Must(template.New("exclude").Parse(excludeTemplate))
-	err := t.Execute(buffer, submodules)
-	if err != nil {
-		return ""
-	}
-
-	return buffer.String()
-}
-
-func InsertInto(original string, insert string, startDetermination string, endDetermination string) (string, error) {
+func StringInsert(original string, insert string, startDetermination string, endDetermination string) (string, error) {
 
 	startDeterminationCount := strings.Count(original, startDetermination)
 	endDeterminationCount := strings.Count(original, endDetermination)
