0814d3bc2f3639d0b181a544fc2c3c606d2e2551
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-04-17 15:21:55 +0200

    ✅ add tests for inserting strings between delimiters

--- utils/string_insert.go
+++ utils/string_insert.go
@@ -1,40 +1,51 @@
 package utils
 
 import (
-	"fmt"
+	"errors"
 	"strings"
 )
 
-func StringInsert(original string, insert string, startDetermination string, endDetermination string) (string, error) {
+func StringInsert(original string, insert string, startDelimiter string, endDelimiter string) (string, error) {
+	return stringInsert(original, insert, startDelimiter, endDelimiter, false)
+}
 
-	startDeterminationCount := strings.Count(original, startDetermination)
-	endDeterminationCount := strings.Count(original, endDetermination)
+func StringInsertAtFirst(original string, insert string, startDelimiter string, endDelimiter string) (string, error) {
+	return stringInsert(original, insert, startDelimiter, endDelimiter, true)
+}
+
+func stringInsert(original string, insert string, startDelimiter string, endDelimiter string, ignoreMultipleOccurrences bool) (string, error) {
+
+	if startDelimiter == "" {
+		return "", errors.New("startDelimiter is empty")
+	}
+
+	if endDelimiter == "" {
+		return "", errors.New("endDelimiter is empty")
+	}
+
+	startDeterminationCount := strings.Count(original, startDelimiter)
+	endDeterminationCount := strings.Count(original, endDelimiter)
 
 	if startDeterminationCount == 0 {
-		return "", fmt.Errorf("cannot to find starting determinator")
+		return "", errors.New("cannot find starting delimiter")
 	}
 
 	if endDeterminationCount == 0 {
-		return "", fmt.Errorf("cannot to find ending determinator")
+		return "", errors.New("cannot find ending delimiter")
 	}
 
-	if startDeterminationCount > 1 {
-		return "", fmt.Errorf("start determinator found multiple times")
+	if !ignoreMultipleOccurrences {
+		if startDeterminationCount > 1 {
+			return "", errors.New("start delimiter found multiple times")
+		}
+
+		if endDeterminationCount > 1 {
+			return "", errors.New("end delimiter found multiple times")
+		}
 	}
 
-	if endDeterminationCount > 1 {
-		return "", fmt.Errorf("end determinator found multiple times")
-	}
-
-	before := strings.Split(original, startDetermination)
-	after := strings.Split(original, endDetermination)
-
-	/*
-		TODO: replace with regex-version
-		// the regex here is untested
-		var re = regexp.MustCompile(`(startDetermination).*(endDetermination)`)
-		s := re.ReplaceAllString(sample, `${1}foo`)
-	*/
+	before := strings.SplitN(original, startDelimiter, 2)
+	after := strings.SplitN(original, endDelimiter, 2)
 
 	return before[0] + insert + after[len(after)-1], nil
 }
