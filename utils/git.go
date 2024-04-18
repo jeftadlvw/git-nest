da7dd57736e070ac1b59faa90cb0547ce23128cf
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-04-18 14:25:26 +0200

    ✨ add new git util functions

--- utils/git.go
+++ utils/git.go
@@ -2,10 +2,92 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"github.com/jeftadlvw/git-nest/models"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
+
+func CloneGitRepository(url string, p models.Path, cloneDirName string) error {
+	url = strings.TrimSpace(url)
+	if url == "" {
+		return fmt.Errorf("git repository url is empty")
+	}
+
+	cloneDirName = strings.TrimSpace(cloneDirName)
+
+	if cloneDirName != "" && filepath.Base(cloneDirName) != cloneDirName {
+		return fmt.Errorf("repository clone directory name may not be path")
+	}
+
+	if !p.Exists() {
+		return fmt.Errorf("%s does not exist", p)
+	}
+
+	commandArgsArr := []string{"clone", "--progress", url}
+	if cloneDirName != "" {
+		commandArgsArr = append(commandArgsArr, cloneDirName)
+	}
+
+	output, err := RunCommandCombinedOutput(p, "git", commandArgsArr...)
+	if err != nil {
+		return fmt.Errorf("error running git clone: %s; output: %s", err, output)
+	}
+
+	if strings.Contains(output, "ERROR: Repository not found.") {
+		return fmt.Errorf("remote repository %s does not exist", url)
+	}
+
+	if strings.Contains(output, "fatal: destination path") {
+		return fmt.Errorf("destination path already exists")
+	}
+
+	return nil
+}
+
+func ChangeGitHead(repository models.Path, head string) error {
+	head = strings.TrimSpace(head)
+	if head == "" {
+		return fmt.Errorf("head is empty")
+	}
+
+	if !repository.Exists() {
+		return fmt.Errorf("%s does not exist", repository)
+	}
+
+	output, err := RunCommandCombinedOutput(repository, "git", "checkout", head, "--progress")
+	if err != nil {
+		return fmt.Errorf("error running git checkout: %s; output: %s", err, output)
+	}
+
+	if strings.Contains(output, "fatal: not a git repository") {
+		return fmt.Errorf("%s is not a git repository", repository)
+	}
+
+	if strings.Contains(output, "error: pathspec") {
+		return fmt.Errorf("head '%s' does not exist", head)
+	}
+
+	return nil
+}
+
+func GetGitRootDirectory(d models.Path) (string, error) {
+	if d.Empty() {
+		return "", errors.New("path to repository may not be empty")
+	}
+
+	path, err := RunCommandCombinedOutput(d, "git", "rev-parse", "--show-toplevel")
+	if err != nil {
+		return "", fmt.Errorf("error running git rev-parse: %s; output: %s", err, path)
+	}
+
+	if strings.HasPrefix(path, "fatal:") {
+		return "", errors.New("git root not found")
+	}
+
+	return path, nil
+}
 
 func GetGitRemoteUrl(d models.Path) (string, error) {
 	path, err := RunCommand(d, "git", "rev-parse", "--show-toplevel")
