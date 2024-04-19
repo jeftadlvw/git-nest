5af38cb810dde034803ee4e3f1992c002d3f7bb7
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-04-19 15:49:07 +0200

    ✅ tested and improved writing to configuration files

---
+++ internal/tests/write_config_test.go
@@ -0,0 +1,335 @@
+package tests
+
+import (
+	"fmt"
+	"github.com/jeftadlvw/git-nest/internal"
+	"github.com/jeftadlvw/git-nest/models"
+	"github.com/jeftadlvw/git-nest/utils"
+	"os"
+	"strings"
+	"testing"
+)
+
+const testRepoUrl = "https://github.com/jeftadlvw/example-repository.git"
+const gitExcludeFile = ".git/info/exclude"
+const gitExcludeStartString string = "# git-nest configuration start"
+const gitExcludeEndString string = "# git-nest configuration end"
+const excludeTemplate string = `# This part influences how git handles nested modules using git-nest.
+# Do not touch except you know what you are doing!`
+
+func TestFmtSubmodulesGitExclude(t *testing.T) {
+	cases := []struct {
+		submodules []models.Submodule
+		expected   string
+	}{
+		{submodules: []models.Submodule{}, expected: ""},
+		{submodules: []models.Submodule{{Path: "submodule1"}}, expected: "submodule1"},
+		{submodules: []models.Submodule{{Path: "submodule1"}, {Path: "submodule2"}, {Path: "submodule3"}}, expected: "submodule1\nsubmodule2\nsubmodule3"},
+	}
+
+	for _, tc := range cases {
+		completeExpectedOutput := excludeTemplate
+		if tc.expected != "" {
+			completeExpectedOutput = completeExpectedOutput + "\n" + tc.expected
+		}
+
+		output := internal.FmtSubmodulesGitExclude(tc.submodules)
+		if output != completeExpectedOutput {
+			t.Errorf("FmtSubmodulesGitExclude() for %v\nExpected:\n>%s<\n\nActual:\n>%s<", tc, completeExpectedOutput, output)
+		}
+	}
+}
+
+func TestWriteSubmodulePathIgnoreConfig(t *testing.T) {
+	tempFile, err := utils.CreateTempFile("")
+	if err != nil {
+		t.Errorf("failed to create temp file")
+		return
+	}
+	defer os.Remove(tempFile.String())
+
+	utils.WriteStrToFile(tempFile, "Foocontent\n\n\t\n")
+
+	// cases are run sequentially on same file
+	cases := []struct {
+		submodules []models.Submodule
+		expected   string
+		error      bool
+	}{
+		{[]models.Submodule{}, "", false},
+		{[]models.Submodule{{Path: "submodule1"}}, "submodule1", false},
+		{[]models.Submodule{{Path: "submodule1"}, {Path: "submodule2"}, {Path: "submodule3"}}, "submodule1\nsubmodule2\nsubmodule3", false},
+		{[]models.Submodule{}, "", false},
+	}
+
+	for _, tc := range cases {
+		expectedContains := excludeTemplate
+		if tc.expected != "" {
+			expectedContains = expectedContains + "\n" + tc.expected
+		}
+		expectedContains = gitExcludeStartString + "\n" + expectedContains + "\n" + gitExcludeEndString
+
+		err = internal.WriteSubmodulePathIgnoreConfig(tempFile, tc.submodules)
+		if tc.error && err == nil {
+			t.Errorf("WriteSubmodulePathIgnoreConfig() for %v returned no error but expected one", tc)
+			continue
+		}
+		if !tc.error && err != nil {
+			t.Errorf("WriteSubmodulePathIgnoreConfig() for %v returned error, but should've not -> %s", tc, err)
+			continue
+		}
+
+		fileContents, err := utils.ReadFileToStr(tempFile)
+		if err != nil {
+			t.Errorf("WriteSubmodulePathIgnoreConfig() failed to read temp file")
+			continue
+		}
+
+		if !strings.Contains(fileContents, expectedContains) {
+			t.Errorf("WriteSubmodulePathIgnoreConfig() for %v expected string in file\nExpected:\n>%s<\n\nFile:\n>%s<", tc, expectedContains, fileContents)
+			continue
+		}
+	}
+}
+
+func TestWriteNestConfig(t *testing.T) {
+	tempFile, err := utils.CreateTempFile("")
+	if err != nil {
+		t.Errorf("failed to create temp file")
+		return
+	}
+	defer os.Remove(tempFile.String())
+
+	tempDir, err := utils.CreateTempDir()
+	if err != nil {
+		t.Errorf("failed to create temp dir: %s", err)
+		return
+	}
+	defer os.RemoveAll(tempDir.String())
+
+	// cases are run sequentially on same file
+	cases := []struct {
+		path       models.Path
+		submodules []models.Submodule
+		error      bool
+	}{
+		{"", []models.Submodule{}, true},
+		{tempDir, []models.Submodule{}, true},
+		{tempFile, []models.Submodule{}, false},
+		{tempFile, []models.Submodule{{Path: "submodule1"}}, false},
+		{tempFile, []models.Submodule{{Path: "submodule1"}, {Path: "submodule2"}, {Path: "submodule3"}}, false},
+		{tempFile, []models.Submodule{}, false},
+	}
+
+	// test without config header in file
+	for _, tc := range cases {
+		expectedContains := internal.SubmodulesToTomlConfig("  ", tc.submodules...)
+
+		err = internal.WriteNestConfig(tc.path, tc.submodules)
+		if tc.error && err == nil {
+			t.Errorf("WriteNestConfig() for %v returned no error but expected one", tc)
+			continue
+		}
+		if !tc.error && err != nil {
+			t.Errorf("WriteNestConfig() for %v returned error, but should've not -> %s", tc, err)
+			continue
+		}
+
+		fileContents, err := utils.ReadFileToStr(tempFile)
+		if err != nil {
+			t.Errorf("WriteNestConfig() failed to read temp file")
+			continue
+		}
+
+		if !strings.Contains(fileContents, expectedContains) {
+			t.Errorf("WriteNestConfig() for %v expected string in file\nExpected:\n>%s<\n\nFile:\n>%s<", tc, expectedContains, fileContents)
+			continue
+		}
+	}
+
+	// add config header and test for it's existence
+	const mockConfig = "[config]\n   foo = \"foo\"\n   bar = \"bar\"\n\t\n\n"
+	const mockConfigInFile = "[config]\n   foo = \"foo\"\n   bar = \"bar\"\n"
+	err = utils.WriteStrToFile(tempFile, mockConfig)
+	if err != nil {
+		t.Errorf("failed to write test config section: %s", err)
+		return
+	}
+	for _, tc := range cases {
+		expectedContains := internal.SubmodulesToTomlConfig("  ", tc.submodules...)
+
+		err = internal.WriteNestConfig(tc.path, tc.submodules)
+		if tc.error && err == nil {
+			t.Errorf("WriteNestConfig() for %v returned no error but expected one", tc)
+			continue
+		}
+		if !tc.error && err != nil {
+			t.Errorf("WriteNestConfig() for %v returned error, but should've not -> %s", tc, err)
+			continue
+		}
+
+		fileContents, err := utils.ReadFileToStr(tempFile)
+		if err != nil {
+			t.Errorf("WriteNestConfig() failed to read temp file")
+			continue
+		}
+
+		if !strings.Contains(fileContents, expectedContains) {
+			t.Errorf("WriteNestConfig() for %v expected string in file\nExpected:\n>%s<\n\nFile:\n>%s<", tc, expectedContains, fileContents)
+			continue
+		}
+
+		if !strings.Contains(fileContents, mockConfigInFile) {
+			t.Errorf("WriteNestConfig() for %v removed config section from config file: %s", tc, fileContents)
+			continue
+		}
+	}
+}
+
+func TestWriteProjectConfigFiles(t *testing.T) {
+
+	prepareGitRepository := func() (models.Path, error) {
+		tempDir, err := utils.CreateTempDir()
+		if err != nil {
+			return "", fmt.Errorf("failed to create temp dir: %w", err)
+		}
+
+		err = utils.CloneGitRepository(testRepoUrl, tempDir, ".")
+		if err != nil {
+			return "", fmt.Errorf("failed to clone git repository: %w", err)
+		}
+
+		return tempDir, nil
+	}
+
+	// git exists
+	t.Run("TestWriteProjectConfigFiles-1", func(t *testing.T) {
+		t.Parallel()
+
+		tempDir, err := prepareGitRepository()
+		if err != nil {
+			t.Errorf("failed to prepare git repository: %s", err)
+			return
+		}
+		defer os.RemoveAll(tempDir.String())
+
+		var configFile = tempDir.SJoin("nestmodules.toml")
+		var absGitExcludeFile = tempDir.SJoin(gitExcludeFile)
+
+		context := models.NestContext{
+			ProjectRoot:       tempDir,
+			GitRepositoryRoot: tempDir,
+			ConfigFileExists:  false,
+			ConfigFile:        configFile,
+			Config:            models.NestConfig{},
+			IsGitInstalled:    true,
+			IsGitRepository:   true,
+		}
+
+		modulesExist := len(context.Config.Submodules) != 0
+
+		gitExcludeWritten, configWritten, gitExcludeWriteErr, configWriteErr := internal.WriteProjectConfigFiles(context)
+
+		if gitExcludeWriteErr != nil {
+			t.Errorf("Writing to git exclude failed: %s", gitExcludeWriteErr)
+		}
+
+		if configWriteErr != nil {
+			t.Errorf("Writing to config file failed: %s", configWriteErr)
+		}
+
+		if !gitExcludeWritten {
+			t.Errorf("WriteProjectConfigFiles() should've written to git exclude")
+		}
+
+		if !configWritten {
+			t.Errorf("WriteProjectConfigFiles() should've written to configuration file")
+		}
+
+		configContents, err := utils.ReadFileToStr(configFile)
+		if err != nil {
+			t.Errorf("error reading configuration file: %s", err)
+		}
+
+		gitExcludeContents, err := utils.ReadFileToStr(absGitExcludeFile)
+		if err != nil {
+			t.Errorf("error reading git exclude file: %s", err)
+		}
+
+		if modulesExist {
+			if !strings.Contains(configContents, internal.SubmodulesToTomlConfig("  ", context.Config.Submodules...)) {
+				t.Errorf("configuration file does not contain submodules")
+			}
+
+			if !strings.Contains(gitExcludeContents, internal.FmtSubmodulesGitExclude(context.Config.Submodules)) {
+				t.Errorf("git exclude file does not contain submodules")
+			}
+		}
+	})
+
+	// git does not exist
+	t.Run("TestWriteProjectConfigFiles-2", func(t *testing.T) {
+		t.Parallel()
+
+		tempDir, err := prepareGitRepository()
+		if err != nil {
+			t.Errorf("failed to prepare git repository: %s", err)
+			return
+		}
+		defer os.RemoveAll(tempDir.String())
+
+		var configFile = tempDir.SJoin("nestmodules.toml")
+		var absGitExcludeFile = tempDir.SJoin(gitExcludeFile)
+
+		context := models.NestContext{
+			ProjectRoot:       tempDir,
+			GitRepositoryRoot: tempDir,
+			ConfigFileExists:  false,
+			ConfigFile:        configFile,
+			Config:            models.NestConfig{},
+			IsGitInstalled:    false, // setting this should be enough!
+			IsGitRepository:   true,
+		}
+
+		modulesExist := len(context.Config.Submodules) != 0
+
+		gitExcludeWritten, configWritten, gitExcludeWriteErr, configWriteErr := internal.WriteProjectConfigFiles(context)
+
+		if gitExcludeWriteErr != nil {
+			t.Errorf("Writing to git exclude failed, and should've never happened: %s", gitExcludeWriteErr)
+		}
+
+		if configWriteErr != nil {
+			t.Errorf("Writing to config file failed: %s", configWriteErr)
+		}
+
+		if gitExcludeWritten {
+			t.Errorf("WriteProjectConfigFiles() should not have written to git exclude")
+		}
+
+		if !configWritten {
+			t.Errorf("WriteProjectConfigFiles() should've written to configuration file")
+		}
+
+		configContents, err := utils.ReadFileToStr(configFile)
+		if err != nil {
+			t.Errorf("error reading configuration file: %s", err)
+		}
+
+		gitExcludeContents, err := utils.ReadFileToStr(absGitExcludeFile)
+		if err != nil {
+			t.Errorf("error reading git exclude file: %s", err)
+		}
+
+		if modulesExist {
+			if !strings.Contains(configContents, internal.SubmodulesToTomlConfig("  ", context.Config.Submodules...)) {
+				t.Errorf("configuration file does not contain submodules")
+			}
+
+			if strings.Contains(gitExcludeContents, internal.FmtSubmodulesGitExclude(context.Config.Submodules)) {
+				t.Errorf("git exclude file does contains submodules")
+			}
+		}
+	})
+
+}
--- internal/write_config.go
+++ internal/write_config.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/jeftadlvw/git-nest/models"
 	"github.com/jeftadlvw/git-nest/utils"
 	"regexp"
@@ -15,54 +16,8 @@ const gitExcludeEndString string = "# git-nest configuration end"
 const excludeTemplate string = `# This part influences how git handles nested modules using git-nest.
 # Do not touch except you know what you are doing!
 {{- range . }}
-	{{ .Path }}
+{{ .Path }}
 {{- end }}`
-
-func WriteConfig(c models.NestContext) {
-
-	// write to git_exclude if project is a git repository
-	if c.IsGitRepository {
-		_ = WriteGitExclude(c.GitRepositoryRoot.SJoin(gitExcludeFile), c.Config.Submodules)
-	}
-
-	// write to git-nest configuration file
-	_ = WriteNestConfig(c.ConfigFile, c.Config.Submodules)
-}
-
-func WriteGitExclude(p models.Path, modules []models.Submodule) error {
-	gitExcludeFileContent := ""
-	submoduleGitExcludePart := FmtSubmodulesGitExclude(modules)
-	submoduleGitExcludePart = gitExcludeStartString + submoduleGitExcludePart + gitExcludeEndString
-
-	existingContent, err := utils.ReadFileToStr(p)
-	if err == nil {
-		gitExcludeFileContent, err = utils.StringInsert(existingContent, submoduleGitExcludePart, gitExcludeStartString, gitExcludeEndString)
-	} else {
-		gitExcludeFileContent = submoduleGitExcludePart
-	}
-
-	err = utils.WriteStrToFile(p, gitExcludeFileContent)
-	return err
-}
-
-func WriteNestConfig(p models.Path, modules []models.Submodule) error {
-	existingContent := ""
-	existingConfig, err := utils.ReadFileToStr(p)
-
-	if err == nil {
-		re := regexp.MustCompile(`\[config](?s:(.*?))(?=\[\[submodule]])`)
-		matches := re.FindStringSubmatch(existingConfig)
-		if len(matches) > 1 {
-			existingContent = matches[1]
-		}
-	}
-
-	submodulesConfig := SubmodulesToTomlConfig("  ", modules...)
-
-	finalConfigContent := strings.TrimSpace(existingContent) + "\n\n" + strings.TrimSpace(submodulesConfig) + "\n"
-	err = utils.WriteStrToFile(p, finalConfigContent)
-	return err
-}
 
 func FmtSubmodulesGitExclude(submodules []models.Submodule) string {
 	buffer := bytes.NewBufferString("")
@@ -73,5 +28,94 @@ func FmtSubmodulesGitExclude(submodules []models.Submodule) string {
 		return ""
 	}
 
-	return buffer.String()
+	return strings.TrimSpace(buffer.String())
+}
+
+func WriteSubmodulePathIgnoreConfig(p models.Path, modules []models.Submodule) error {
+	submoduleGitExcludePart := FmtSubmodulesGitExclude(modules)
+	submoduleGitExcludePart = gitExcludeStartString + "\n" + submoduleGitExcludePart + "\n" + gitExcludeEndString
+
+	fileContent := submoduleGitExcludePart
+
+	existingContent, err := utils.ReadFileToStr(p)
+	if err == nil {
+		localFileContent, localErr := utils.StringInsertAtFirst(existingContent, submoduleGitExcludePart, gitExcludeStartString, gitExcludeEndString)
+		if localErr == nil {
+			fileContent = localFileContent
+		}
+	}
+
+	err = utils.WriteStrToFile(p, fileContent)
+	return err
+}
+
+func WriteNestConfig(p models.Path, modules []models.Submodule) error {
+	existingContent := ""
+	existingConfig := ""
+
+	if p.Empty() {
+		return fmt.Errorf("cannot write to empty path")
+	}
+
+	if p.IsDir() {
+		return fmt.Errorf("passed path is a directory: %s", p)
+	}
+
+	if p.IsFile() {
+		localExistingConfig, err := utils.ReadFileToStr(p)
+		if err != nil {
+			return fmt.Errorf("cannot read existing config: %w", err)
+		}
+		existingConfig = localExistingConfig
+
+		// append '[[submodule]]' to in-memory config for regex to find existing config
+		// even if that header already exists, it'll be removed anyway by later processing
+		existingConfig = existingConfig + "[[submodule]]"
+	}
+
+	// find any existing config section
+	re := regexp.MustCompile(`\[config](?s:(.*?))\[\[submodule]]`)
+	matches := re.FindStringSubmatch(existingConfig)
+
+	if len(matches) > 1 {
+		existingContent = strings.TrimSpace(strings.TrimSuffix(strings.TrimSpace(matches[0]), "[[submodule]]"))
+	}
+
+	submodulesConfig := SubmodulesToTomlConfig("  ", modules...)
+
+	if existingContent != "" {
+		existingContent = existingContent + "\n\n"
+	}
+
+	finalConfigContent := existingContent + submodulesConfig + "\n"
+	err := utils.WriteStrToFile(p, finalConfigContent)
+	if err != nil {
+		return fmt.Errorf("cannot write 'nestmodules.toml': %w", err)
+	}
+
+	return nil
+}
+
+func WriteProjectConfigFiles(c models.NestContext) (bool, bool, error, error) {
+
+	var gitExcludeWritten, configWritten bool
+	var gitExcludeWriteError, configWriteError error
+
+	// write to git_exclude if project is a git repository
+	if c.IsGitInstalled && c.IsGitRepository {
+		err := WriteSubmodulePathIgnoreConfig(c.GitRepositoryRoot.SJoin(gitExcludeFile), c.Config.Submodules)
+		if err == nil {
+			gitExcludeWritten = true
+		}
+		gitExcludeWriteError = err
+	}
+
+	// write to git-nest configuration file
+	err := WriteNestConfig(c.ConfigFile, c.Config.Submodules)
+	if err == nil {
+		configWritten = true
+	}
+	configWriteError = err
+
+	return gitExcludeWritten, configWritten, gitExcludeWriteError, configWriteError
 }
