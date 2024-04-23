541338dae342cdaad3f18f03bda0e4bc475565dc
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-04-23 18:53:48 +0200

    ✨ add custom wrapper around cobra.Command's Run field that includes custom arg count validation

--- cmd/internal/funcs.go
+++ cmd/internal/funcs.go
@@ -1,9 +1,36 @@
 package internal
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
+/*
+PrintUsage is a wrapper function around the default cobra.Command Usage() function.
+*/
 func PrintUsage(cmd *cobra.Command, args []string) {
 	_ = cmd.Usage()
+}
+
+/*
+RunWrapper wraps the function set for the 'Run' field in a cobra.Command.
+It takes a runner function and an argument count validation function. If the latter
+is not nil, it is executed first and checked for returned errors. If no errors
+were returned, the runner function is executed.
+*/
+func RunWrapper(run func(args []string), validateArgCount ...func(c int) error) func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		if validateArgCount != nil {
+
+			for _, validationFunc := range validateArgCount {
+				err := validationFunc(len(args))
+				if err != nil {
+					fmt.Printf("fatal: argument count error: %s\n", err)
+					return
+				}
+			}
+		}
+
+		run(args)
+	}
 }
