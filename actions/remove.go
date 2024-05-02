40f3ed5305dc14b6001068a80aec196de77714a9
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-05-02 12:58:38 +0200

    🚧 implement `remove` command

--- actions/remove.go
+++ actions/remove.go
@@ -1,16 +1,82 @@
 package actions
 
 import (
-	"errors"
+	"fmt"
+	"github.com/jeftadlvw/git-nest/internal"
 	"github.com/jeftadlvw/git-nest/models"
+	"github.com/jeftadlvw/git-nest/utils"
+	"os"
+	"slices"
 )
 
 /*
 RemoveSubmoduleFromContext is a high-level wrapper that removes a submodule from the context.
 */
-func RemoveSubmoduleFromContext(context *models.NestContext, s string) error {
-	var err error
-	err = errors.New("not implemented")
+func RemoveSubmoduleFromContext(context *models.NestContext, p models.Path, removeDir bool, forceDelete bool) error {
 
-	return err
+	relativeToRoot, err := internal.PathRelativeToRootButOtherOriginIfNotAbs(context.ProjectRoot, context.WorkingDirectory, p)
+	if err != nil {
+		return fmt.Errorf("internal error: could not find relative to project root: %w", err)
+	}
+
+	// check if relative path escapes project root
+	if internal.PathContainsUp(relativeToRoot) {
+		return fmt.Errorf("validation error: %s escapes the project root", p)
+	}
+
+	absolutePath := context.ProjectRoot.Join(relativeToRoot)
+	if absolutePath == context.ProjectRoot {
+		return fmt.Errorf("validation error: path cannot be project root")
+	}
+
+	// check if passed submodule exists in context
+	var removeIndex int = -1
+	for i, submodule := range context.Config.Submodules {
+		if submodule.Path.String() == absolutePath.String() {
+			removeIndex = i
+		}
+	}
+
+	// return error if no match found
+	if removeIndex == -1 {
+		return fmt.Errorf("passed submodule does not exist")
+	}
+
+	// check if directory exists
+	if absolutePath.IsDir() {
+		if absolutePath.BContains("*") && !removeDir {
+			return fmt.Errorf("submodule directry at %s is not empty.\nUse git-nest rm [path] -d to remove it.", p)
+		}
+
+		// check if repository has untracked changes
+		hasUncommittedChanges, err := utils.GetGitHasUncommittedChanges(absolutePath)
+		if err != nil {
+			return fmt.Errorf("internal error: could not check if uncommitted changes exist: %w", err)
+		}
+
+		if hasUncommittedChanges && !forceDelete {
+			return fmt.Errorf("submodule repository at %s contains uncommitted changes.\nCommit and push your changes or use git-nest rm [path] -df to forcefully remove it.", p)
+		}
+
+		// check if repository has unpublished changes
+		hasUnpushedCommits, err := utils.GetGitHasUnpushedCommits(absolutePath)
+		if err != nil {
+			return fmt.Errorf("internal error: could not check if unpushed commits exist: %w", err)
+		}
+
+		if hasUnpushedCommits && !forceDelete {
+			return fmt.Errorf("submodule repository at %s has unpushed commits.\nPush your changes or use git-nest rm [path] -df to forcefully remove it.", p)
+		}
+
+		// delete directory
+		err = os.RemoveAll(absolutePath.String())
+		if err != nil {
+			return fmt.Errorf("internal error: error while deleting %s: %w", absolutePath.String(), err)
+		}
+	}
+
+	// remove submodule from submodule slice
+	context.Config.Submodules = slices.Delete(context.Config.Submodules, removeIndex+1, removeIndex+2)
+
+	return nil
 }
--- cmd/remove.go
+++ cmd/remove.go
@@ -5,28 +5,34 @@ import (
 	"github.com/jeftadlvw/git-nest/actions"
 	cmdInternal "github.com/jeftadlvw/git-nest/cmd/internal"
 	"github.com/jeftadlvw/git-nest/internal"
+	"github.com/jeftadlvw/git-nest/models"
 	"github.com/spf13/cobra"
 )
 
 func createRemoveCommand() *cobra.Command {
-	var infoCmd = &cobra.Command{
-		Use:     "remove path | url",
+	var rmCmd = &cobra.Command{
+		Use:     "remove [path]",
 		Aliases: []string{"rm"},
 		Short:   "Remove a submodule from this project",
 		Run:     cmdInternal.RunWrapper(wrapRemoveSubmodule, cmdInternal.ArgExactN(1)),
 	}
 
-	return infoCmd
+	rmCmd.Flags().BoolP("delete", "d", false, "delete existing directory")
+	rmCmd.Flags().BoolP("force", "f", false, "force delete existing directory")
+
+	return rmCmd
 }
 
-func wrapRemoveSubmodule(args []string) {
-	err := removeSubmodule(args[0])
+func wrapRemoveSubmodule(cmd *cobra.Command, args []string) {
+	deleteDirectory, _ := cmd.Flags().GetBool("delete")
+	forceDelete, _ := cmd.Flags().GetBool("force")
+	err := removeSubmodule(models.Path(args[0]), deleteDirectory, forceDelete)
 	if err != nil {
 		fmt.Printf("error: %s\n", err)
 	}
 }
 
-func removeSubmodule(s string) error {
+func removeSubmodule(p models.Path, deleteDirectory bool, forceDelete bool) error {
 
 	// read context
 	context, err := internal.EvaluateContext()
@@ -34,7 +40,7 @@ func removeSubmodule(s string) error {
 		return fmt.Errorf("internal context error: %w.\nPlease fix any configuration errors to proceed", err)
 	}
 
-	err = actions.RemoveSubmoduleFromContext(&context, s)
+	err = actions.RemoveSubmoduleFromContext(&context, p, deleteDirectory, forceDelete)
 	if err != nil {
 		return err
 	}
