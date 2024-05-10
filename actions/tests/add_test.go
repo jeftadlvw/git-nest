10bace6d0c991d85d0aa623ce417fba3c9d2a24c
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-05-10 20:16:08 +0200

    ✅ fix errors for action 'add'

--- actions/tests/add_test.go
+++ actions/tests/add_test.go
@@ -3,26 +3,34 @@ package tests
 import (
 	"fmt"
 	"github.com/jeftadlvw/git-nest/actions"
+	"github.com/jeftadlvw/git-nest/interfaces"
 	"github.com/jeftadlvw/git-nest/internal"
+	"github.com/jeftadlvw/git-nest/migrations"
+	mcontext "github.com/jeftadlvw/git-nest/migrations/context"
+	"github.com/jeftadlvw/git-nest/migrations/git"
 	"github.com/jeftadlvw/git-nest/models"
 	"github.com/jeftadlvw/git-nest/models/urls"
 	"github.com/jeftadlvw/git-nest/test_env"
 	test_env_models "github.com/jeftadlvw/git-nest/test_env/models"
 	"github.com/jeftadlvw/git-nest/utils"
 	"os"
+	"reflect"
 	"testing"
 )
 
 func TestAddSubmoduleInContext(t *testing.T) {
 
-	testRepoUrl, err := urls.HttpUrlFromString(test_env.RepoUrl)
-	if err != nil {
-		t.Fatal(err)
+	testRepoUrl, lerr := urls.HttpUrlFromString(test_env.RepoUrl)
+	if lerr != nil {
+		t.Fatal(lerr)
 	}
 
 	testFile := "testfileinrepo"
 	testDirEmpty := "testdirinrepoempty"
 	testDirFull := "testdirinrepofull"
+
+	expectedMigrations := []interfaces.Migration{mcontext.AppendSubmodule{}, git.Clone{}}
+	expectedMigrationsRef := []interfaces.Migration{mcontext.AppendSubmodule{}, git.Clone{}, git.Checkout{}}
 
 	cases := []struct {
 		url                      string
@@ -31,34 +39,35 @@ func TestAddSubmoduleInContext(t *testing.T) {
 		submodules               []models.Submodule
 		simulateNoGitInstalled   bool
 		simulateDuplicateOrigins bool
+		expectedMigrations       []interfaces.Migration
 		err                      bool
 	}{
-		{test_env.RepoUrl, "", "", []models.Submodule{}, true, false, true},
-		{test_env.RepoUrl, "", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, testFile, "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, testFile + "/", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, testDirEmpty, "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, testDirEmpty + "/", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, testDirFull, "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, testDirFull + "/", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, "../foo", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, "foo/bar/../", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, "foo/bar/..", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, "foo/bar/../..", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, "foo/bar/../../", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, "foo/bar/../../foo", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, "foo", "", []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, "f!oo", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, "f!oo", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, "fo*o", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, "/foo", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, "/../foo", "", []models.Submodule{}, false, false, true},
-		{test_env.RepoUrl, "foo", test_env.RepoBranch1, []models.Submodule{}, false, false, false},
-		{test_env.RepoUrl, "", "", []models.Submodule{{"example-repository", testRepoUrl, ""}}, false, false, true},
-		{test_env.RepoUrl, "", "", []models.Submodule{{"example-repository", testRepoUrl, ""}}, false, true, true},
-		{test_env.RepoUrl, "", test_env.RepoBranch1, []models.Submodule{{"foo", testRepoUrl, ""}}, false, false, true},
-		{test_env.RepoUrl, "", test_env.RepoBranch1, []models.Submodule{{"foo", testRepoUrl, ""}}, false, true, false},
-		{test_env.RepoUrl, "", test_env.RepoBranch1, []models.Submodule{{"foo", testRepoUrl, test_env.RepoBranch1}}, false, true, false},
+		{test_env.RepoUrl, "", "", []models.Submodule{}, true, false, nil, true},
+		{test_env.RepoUrl, "", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, testFile, "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, testFile + "/", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, testDirEmpty, "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, testDirEmpty + "/", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, testDirFull, "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, testDirFull + "/", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, "../foo", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, "foo/bar/../", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, "foo/bar/..", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, "foo/bar/../..", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, "foo/bar/../../", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, "foo/bar/../../foo", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, "foo", "", []models.Submodule{}, false, false, expectedMigrations, false},
+		{test_env.RepoUrl, "f!oo", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, "f!oo", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, "fo*o", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, "/foo", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, "/../foo", "", []models.Submodule{}, false, false, nil, true},
+		{test_env.RepoUrl, "foo", test_env.RepoBranch1, []models.Submodule{}, false, false, expectedMigrationsRef, false},
+		{test_env.RepoUrl, "", "", []models.Submodule{{"example-repository", testRepoUrl, ""}}, false, false, nil, true},
+		{test_env.RepoUrl, "", "", []models.Submodule{{"example-repository", testRepoUrl, ""}}, false, true, nil, true},
+		{test_env.RepoUrl, "", test_env.RepoBranch1, []models.Submodule{{"foo", testRepoUrl, ""}}, false, false, nil, true},
+		{test_env.RepoUrl, "", test_env.RepoBranch1, []models.Submodule{{"foo", testRepoUrl, ""}}, false, true, expectedMigrationsRef, false},
+		{test_env.RepoUrl, "", test_env.RepoBranch1, []models.Submodule{{"foo", testRepoUrl, test_env.RepoBranch1}}, false, true, expectedMigrationsRef, false},
 	}
 
 	for index, tc := range cases {
@@ -111,15 +120,33 @@ func TestAddSubmoduleInContext(t *testing.T) {
 			}
 
 			cloneDir := tc.cloneDir
-			err = actions.AddSubmoduleInContext(&context, url, tc.ref, models.Path(cloneDir))
+			migrationArr, err := actions.AddSubmoduleInContext(&context, url, tc.ref, models.Path(cloneDir))
 
-			// check things
+			// check migration array
+			if !tc.err && len(tc.expectedMigrations) != len(migrationArr) {
+				t.Fatalf("AddSubmoduleInContext() for case %d returned unequal amounts of migrations: expected %d, got %d", index+1, len(tc.expectedMigrations), len(migrationArr))
+			}
+			if !tc.err {
+				for mindex, migration := range migrationArr {
+					if reflect.TypeOf(migration) != reflect.TypeOf(tc.expectedMigrations[mindex]) {
+						t.Fatalf("AddSubmoduleInContext() for case %d had unexpected migration at index %d", index+1, mindex)
+					}
+				}
+			}
+
+			// run migrations if action function call did not return an error
+			if err == nil {
+				err = migrations.RunMigrations(migrationArr...)
+			}
+
+			// test for errors
 			if tc.err && err == nil {
-				t.Errorf("AddSubmoduleInContext() for case %d returned no error but expected one", index+1)
+				t.Fatalf("AddSubmoduleInContext() for case %d returned no error but expected one", index+1)
 			}
 			if !tc.err && err != nil {
-				t.Errorf("AddSubmoduleInContext() for case %d returned error, but should've not -> %s", index+1, err)
+				t.Fatalf("AddSubmoduleInContext() for case %d returned error, but should've not -> %s", index+1, err)
 			}
+
 		})
 	}
 }
