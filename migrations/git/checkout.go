07890b6bf8de6bee7827bf3b78d11cc469514e77
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-05-10 16:45:22 +0200

    ♻️ remove validation, as they've been moved to utils.GitCheckout

--- migrations/git/checkout.go
+++ migrations/git/checkout.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/jeftadlvw/git-nest/models"
 	"github.com/jeftadlvw/git-nest/utils"
-	"strings"
 )
 
 type Checkout struct {
@@ -13,21 +12,7 @@ type Checkout struct {
 }
 
 func (m Checkout) Migrate() error {
-
-	if !m.Path.Exists() {
-		return fmt.Errorf("%s does not exist", m.Path)
-	}
-
-	if !m.Path.IsDir() {
-		return fmt.Errorf("%s is not a directory", m.Path)
-	}
-
-	ref := strings.TrimSpace(m.Ref)
-	if ref == "" {
-		return fmt.Errorf("ref cannot be blank")
-	}
-
-	err := utils.ChangeGitHead(m.Path, ref)
+	err := utils.GitCheckout(m.Path, m.Ref)
 	if err != nil {
 		return fmt.Errorf("error while changing ref: %s", err)
 	}
