9419c4ecaa0f2cab1074caf9c830de9e1401a92e
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-05-16 21:50:40 +0200

    ♻️ move directory creation into migration

--- migrations/git/clone.go
+++ migrations/git/clone.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jeftadlvw/git-nest/interfaces"
 	"github.com/jeftadlvw/git-nest/models"
 	"github.com/jeftadlvw/git-nest/utils"
+	"os"
 )
 
 type Clone struct {
@@ -14,6 +15,14 @@ type Clone struct {
 }
 
 func (m Clone) Migrate() error {
+
+	if !m.Path.Exists() {
+		err := os.MkdirAll(m.Path.String(), os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("internal error: could not create directory %s: %w", m.Path, err)
+		}
+	}
+
 	err := utils.CloneGitRepository(m.Url.String(), m.Path, m.CloneDirName)
 	if err != nil {
 		return fmt.Errorf("error while cloning into %s: %s", m.Path.SJoin(m.CloneDirName), err)
