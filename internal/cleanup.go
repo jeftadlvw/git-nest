b6e4a29a1cc6a248b532d086337c3be474a03434
jeftadlvw <143009333+jeftadlvw@users.noreply.github.com>
2024-05-20 20:29:37 +0200

    ✨ finally some more functionality for Cleanup()
    (fr tho, there's a cleanup-stack now!)

--- internal/cleanup.go
+++ internal/cleanup.go
@@ -1,7 +1,37 @@
 package internal
 
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+var cleanupMutex sync.Mutex
+var cleanupStack []func() error
+
 /*
 Cleanup is responsible for cleaning up any left-over files and things.
 Gets called by the main function on exiting.
 */
-func Cleanup() {}
+func Cleanup() {
+	cleanupMutex.Lock()
+
+	var err error
+	for _, f := range cleanupStack {
+		err = f()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "error while cleaning up: %s", err)
+		}
+	}
+
+	cleanupMutex.Unlock()
+}
+
+/*
+AddCleanup adds a function to the cleanup stack.
+*/
+func AddCleanup(f func() error) {
+	cleanupMutex.Lock()
+	cleanupStack = append(cleanupStack, f)
+	cleanupMutex.Unlock()
+}
